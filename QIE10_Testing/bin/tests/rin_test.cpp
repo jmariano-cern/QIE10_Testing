ddd8153a1885dac6f37e96ae169d30e8e45cfecd
Danny <dnoonan@cern.ch>
2016-09-02 11:16:15 +0200

    Switch to very simple test of rinSel functionality

--- QIE10_Testing/bin/tests/rin_test.cpp
+++ QIE10_Testing/bin/tests/rin_test.cpp
@@ -35,6 +35,7 @@ void rin_test(Int_t run_num) {
   char hist0_name[512]; 
   char hist1_name[512];
   char file0_name[512];
+  char outputfile0_name[512];
   char dir_name[512];
   char figure0_name[512];
   char figure1_name[512];
@@ -43,11 +44,16 @@ void rin_test(Int_t run_num) {
   system(dir_name);
   sprintf(dir_name,"mkdir ../../img/%i/rin_test",run_num);
   system(dir_name);
+  sprintf(dir_name,"mkdir ../../img/%i/rin_test/rootFiles",run_num);
+  system(dir_name);
 
   TH2F *h0_temp = new TH2F();
   TProfile *p0_temp = new TProfile();
+  TH2F *h1_temp = new TH2F();
+  TProfile *p1_temp = new TProfile();
   TF1 *fitFunction = new TF1();
   TFile *_file0 =  new TFile();
+  TFile *output_file = new TFile();
   double rinSlope = 0;
   double rinOffset = 0;
   
@@ -74,10 +80,13 @@ void rin_test(Int_t run_num) {
     if (lv0_mask[h] == 1) {
       for (Int_t s = 0 ; s < SL_num; s++) {
 	if (lv1_mask[h][s] == 1) {
+	  sprintf(outputfile0_name,"../../img/%i/rin_test/rootFiles/RinSelTest_HF%i_Slot%i.root",run_num,h+1,s+1);
+	  output_file = new TFile(outputfile0_name,"RECREATE");
 	  for (Int_t q = 0; q < QI_num; q++) {
 	    if (lv2_mask[h][s][q] == 1) {
+	      sprintf(dir_name,"QIE%i",q+1);
+	      output_file->mkdir(dir_name);
 	      sprintf(hist0_name,"%s/%s_HF%i_Slot%i_QIE%i","refabs_vs_rin_CH","refabs_vs_rin_CH",h+1,s+1,q+1);
-	      cout << hist0_name << endl;
 	      h0_temp = (TH2F*)_file0->Get(hist0_name);
 	      p0_temp = (TProfile*) h0_temp->ProfileX();
 	      p0_temp->Fit("pol1","Q");
@@ -85,54 +94,55 @@ void rin_test(Int_t run_num) {
 	      double rinSlope = fitFunction->GetParameter(1);
 	      double rinOffset = fitFunction->GetParameter(0);
 	      
-	      // cout << "HF: " << h+1 << ", SL: " << s+1 << ", QI: " << q+1 << " -- TurnOnPoint: " << vbs_top << ", a(x-top)^2: " << vbs_a << ", +b: " << vbs_b << endl;
 	      sprintf(figure0_name,"../../img/%i/rin_test/scan_HF%i_SL%i_QI%i.png",run_num,h+1,s+1,q+1);
-	      //h0_temp->SetTitle("Bias Voltage Scan with Sequencer");
 	      h0_temp->GetXaxis()->SetTitle("RinSel Value");
 	      h0_temp->GetYaxis()->SetTitle("Charge in Reflection (fC)");
 	      h0_temp->GetYaxis()->SetTitleOffset(1.5);
-	      c1->SaveAs(figure0_name);
-	      c1->Clear();
 
-	      if ((rinSlope < 0.01)){
-		lv2_err_map_slope[h][s][q] = 0;
-		lv2_err_map_gen[h][s][q] = 0;
-	      }	
+	      sprintf(hist1_name,"%s/%s_HF%i_Slot%i_QIE%i","refrat_vs_rin_CH","refrat_vs_rin_CH",h+1,s+1,q+1);
+	      h1_temp = (TH2F*)_file0->Get(hist1_name);
+	      p1_temp = (TProfile*) h1_temp->ProfileX();
 
-	      if ((rinOffset>0) || ((rinSlope*15+rinOffset)<0)) {
-		lv2_err_map_zeropoint[h][s][q] = 0;
-		lv2_err_map_gen[h][s][q] = 0;
-	      }	
+	      // if ((rinSlope < 0.01) || (rinOffset>0) || ((rinSlope*15+rinOffset)<0)){
+	      // 	c1->SaveAs(figure0_name);
+	      // 	c1->Clear();
+	      // }
+	      // if ((rinSlope < 0.01)){
+	      // 	lv2_err_map_slope[h][s][q] = 0;
+	      // 	lv2_err_map_gen[h][s][q] = 0;
+	      // }	
 
+	      // if ((rinOffset>0) || ((rinSlope*15+rinOffset)<0)) {
+	      // 	lv2_err_map_zeropoint[h][s][q] = 0;
+	      // 	lv2_err_map_gen[h][s][q] = 0;
+	      // }	
 
-	      // sprintf(hist1_name,"%s/%s_HF%i_Slot%i_QIE%i","qratio2_vb_PR","qratio2_vb_PR",h+1,s+1,q+1);
-	      // h1_temp = (TH1F*)_file0->Get(hist1_name);
-	      // sprintf(figure1_name,"../../img/%i/vbs_test/ratio2_HF%i_SL%i_QI%i.png",run_num,h+1,s+1,q+1);
-	      // //h0_temp->SetTitle("Bias Voltage Scan with Sequencer");
-	      // h1_temp->GetXaxis()->SetTitle("LED Bias Voltage (V)");
-	      // h1_temp->GetYaxis()->SetTitle("(Qmax+Qnext)/Qsum");
-	      // h1_temp->GetYaxis()->SetTitleOffset(1.5);
-	      // h1_temp->Draw();
-	      // c1->SaveAs(figure1_name);
-	      // c1->Clear();
+	      if (p1_temp->GetBinContent(1) > p1_temp->GetBinContent(15)){
+	      	lv2_err_map_gen[h][s][q] = 0;
+	      }
+	      output_file->cd();
+	      sprintf(dir_name,"QIE%i",q+1);
+	      output_file->cd(dir_name);
+	      h0_temp->Write();
+	      p0_temp->Write();
+	      h1_temp->Write();
+	      p1_temp->Write();
 
-	      /*
-	      if ((ped_slope > ped_slope_high) || (ped_slope < ped_slope_low) || (h1_temp->GetEntries() < 10)) {
-		lv2_err_map_slope[h][s][q] = 0;
-		lv2_err_map_gen[h][s][q] = 0;
-	      }	
-	      */
 	      h0_temp->Delete();
 	      p0_temp->Delete();
+	      h1_temp->Delete();
+	      p1_temp->Delete();
 	    }
 	  }
+	  output_file->Write();
+	  output_file->Close();
 	}
       }
     }
   }
   
-  draw_map(lv2_err_map_slope, run_num, "rin_test", "RinScanSlope");
-  draw_map(lv2_err_map_zeropoint, run_num, "rin_test", "RinScanZeroPoint");
+  // draw_map(lv2_err_map_slope, run_num, "rin_test", "RinScanSlope");
+  // draw_map(lv2_err_map_zeropoint, run_num, "rin_test", "RinScanZeroPoint");
   draw_map(lv2_err_map_gen, run_num, "rin_test", "RinScanAll");
   
 } // close function
