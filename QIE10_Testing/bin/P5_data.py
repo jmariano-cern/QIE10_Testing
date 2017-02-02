837dc4cdbf2bff6edf9cc60a95adba9baab92e14
jmariano <jmariano@terpmail.umd.edu>
2017-02-02 14:46:07 +0100

    updating P5 analyzer -- still testing

--- QIE10_Testing/bin/P5_data.py
+++ QIE10_Testing/bin/P5_data.py
@@ -20,7 +20,7 @@ if ((len(sys.argv) < 3) or (len(sys.argv) > 5)):
 #
 #   Change the filename to process
 #
-#runNumber = sys.argv[2]
+runNumber = sys.argv[2]
 
 #if runNumber not in os.listdir('../img'):
 #    os.makedirs('../img/' + runNumber)
@@ -33,7 +33,7 @@ process.source = cms.Source("PoolSource",
         #'/store/data/Run2016B/JetHT/RAW/v2/000/275/309/00000/30B76086-B534-E611-A108-02163E0135C7.root'        
         #'/store/data/Run2016B/JetHT/RAW/v2/000/275/309/00000/1EC2D35B-E734-E611-B26F-02163E014393.root'
         #'/store/data/Run2016B/JetHT/RAW/v2/000/275/310/00000/0430BB00-BB35-E611-8343-02163E0135D2.root'
-        'root://eoscms.cern.ch//eos/cms/store/data/Run2016B/ZeroBias1/RAW/v1/000/272/022/00000/FC0DE82C-BE0D-E611-8422-02163E01389A.root'
+        'root://eoscms.cern.ch//eos/cms/store/group/dpg_hcal/comm_hcal/USC/run' + runNumber + '/USC_'+ runNumber + '.root'
     )
 )
 
@@ -63,7 +63,8 @@ process.hcalDigis = cms.EDProducer("HcalRawToDigi",
                                    ComplainEmptyData = cms.untracked.bool(False),
                                    #       UnpackCalib = cms.untracked.bool(True),
                                    FEDs = cms.untracked.vint32(
-                                   1118, 1120, 1122, 1132,
+                                   # 1118, 1120, 1122, 
+                                   1132,
                                    61,63,
                                    700, 701, 702, 703, 704,
                                    705, 706, 707, 708, 709,
@@ -71,7 +72,10 @@ process.hcalDigis = cms.EDProducer("HcalRawToDigi",
                                    715, 716, 717, 718, 719,
                                    720, 721, 722, 723, 724,
                                    725, 726, 727, 728, 729,
-                                   730, 731, 732
+                                   730, 731, 732,
+                                   1118, 1119, # c22
+                                   1120, 1121, # c29
+                                   1122, 1123, # c32
                                    ),
                                    firstSample = cms.int32(0),
 #                                   lastSample = cms.int32(10)
@@ -80,8 +84,8 @@ process.hcalDigis = cms.EDProducer("HcalRawToDigi",
 
 
 suite_code = 0
-if len(sys.argv) > 2:
-    suite_code = int(sys.argv[2])
+if len(sys.argv) > 3:
+    suite_code = int(sys.argv[3])
 
 sequencer_flag = 0
 #if len(sys.argv) == 5:
