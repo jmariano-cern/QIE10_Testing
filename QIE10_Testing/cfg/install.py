fa3ae098b9b2d135441d882a3c3d0cddea15e346
jmariano <jmariano@terpmail.umd.edu>
2017-02-07 11:11:07 +0100

    adjusting install script for new mapping scheme -- default: enable full detector

--- QIE10_Testing/cfg/install.py
+++ QIE10_Testing/cfg/install.py
@@ -20,7 +20,7 @@ call('mkdir /afs/cern.ch/work/' + user()[0] + '/' + user() + '/public/904_runs')
 
 print 'Building map files...'
 cd(env['QIE10ROOT'] + '/src')
-call('./remap')
+call('./remap HFM01,HFM02,HFM03,HFM04,HFM05,HFM06,HFM07,HFM08,HFP01,HFP02,HFP03,HFP04,HFP05,HFP06,HFP07,HFP08')
 
 print 'Building the analyzer core...'
 call('python generate.py')
@@ -30,12 +30,4 @@ print 'Compiling the analyzer...'
 call('scram b -j 8')
 print 'Analyzer compiled.'
 
-print '#######################################################################'
-print '#######################################################################'
-print 'Acquiring the raw data. Please enter your password for the 904 cluster.'
-print "(If you don't want to update the runs, you can cancel with CTRL+C)"
-
-cd(env['QIE10ROOT'] + '/bin')
-call('./update_runs')
 print 'Installion complete.'
-
