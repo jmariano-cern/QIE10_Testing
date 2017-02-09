424fcc6c24d427a64b4505bbfa091868be52653c
jmariano <jmariano@terpmail.umd.edu>
2017-02-09 16:22:44 +0100

    modified installer

--- QIE10_Testing/cfg/install.py
+++ QIE10_Testing/cfg/install.py
@@ -6,18 +6,6 @@ from os import environ as env
 from getpass import getuser as user
 from sys import exit
 
-yn = 0
-
-print('Before installation, you MUST have enabled your lxplus work area.')
-yn = raw_input('Is your work area enabled? -- ')
-
-if ((yn != 'Yes') and (yn != 'yes')and (yn != 'Y') and (yn != 'y') and (yn != 'OK')and (yn != 'Ok') and (yn != 'ok') and (yn != '1')):
-    print 'Installation failed. Please enable your work area (see: https://resources.web.cern.ch/resources/Help/?kbid=067040) and try again.'
-    exit()
-
-print 'Creating folder to store raw run data: /afs/cern.ch/work/' + user()[0] + '/' + user() + '/public/904_runs...'
-call('mkdir /afs/cern.ch/work/' + user()[0] + '/' + user() + '/public/904_runs')
-
 print 'Building map files...'
 cd(env['QIE10ROOT'] + '/src')
 call('./remap HFM01,HFM02,HFM03,HFM04,HFM05,HFM06,HFM07,HFM08,HFP01,HFP02,HFP03,HFP04,HFP05,HFP06,HFP07,HFP08')
